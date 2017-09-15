e907a28581a46ac71c139eea5913836088aec0bc
MarekSteinke <30751697+MarekSteinke@users.noreply.github.com>
2017-09-15 15:11:03 +0200

    test added

--- control_ambiguity/tests.py
+++ control_ambiguity/tests.py
@@ -7,5 +7,12 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (views.MyPage)
-        yield (views.Results)
+        yield(views.Introduction)
+        yield (views.Choice, {'wtp_remove': 5, 'expected_green_balls': 5})
+        yield(views.Information)
+        if self.player.id_in_group == 1:
+            yield (views.Input, {'no_modification_ball': "green", 'one_modified_ball': "green", 'five_modified_balls': "green"})
+        else:
+        	pass
+        yield(views.Results)
+        yield(views.Demographics, {'age': 55, 'gender': "male", 'risk': 4, 'country': "AT"})
